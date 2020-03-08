459b66d9cafc3f66d951ee4910a4acfd89b72bd5
WuShaoling <wsl6@outlook.com>
2020-03-08 15:45:42 +0800

    first commit
    Signed-off-by: WuShaoling <wsl6@outlook.com>

---
+++ README.md
@@ -0,0 +1,98 @@
+# open-md-lint
+
+## Description
+
+`open-md-lint` is an open source module for markdown file format checking.
+
+## Getting start
+
+### Install
+
+`npm i open-md-lint`
+
+### Demo
+
+package.json
+
+```json
+{
+  "scripts": {
+    "md-lint": "open-md-lint"
+  }
+}
+```
+
+```shell
+# npm run md-lint
+```
+
+## Docs
+
+### The definition of Config
+
+```typescript
+interface Config {
+  requires?: string;
+  patterns: string[];
+  options: {
+    useGitIgnore?: boolean;
+    usePackageJson?: boolean;
+    configKey?: string;
+    gitIgnoreFile?: string;
+    ignore?: string[];
+    cwd?: string;
+  };
+}
+```
+
+- requires(optional): Path to the js file containing the user deinfined rules, the default configuration is declare in the src/default_requires.js
+- For the rest configuration, please refer to [deglob](https://www.npmjs.com/package/deglob)
+
+#### Default config
+
+```javascript
+{
+  patterns: [ '**/*.md' ],
+  options: {
+    useGitIgnore: true,
+    ignore: [ 'node_modules/**/*' ],
+  },
+}
+```
+
+#### Customer configuration
+
+Note: In order to facilitate the user to integrate the configuration into the package.json file, after loading the configuration file, the configuration will be read from the 'open-md-lint' field, so the specific configuration should be included in the 'open-md-lint' field, for example:
+
+```json
+{
+  "open-md-lint": {
+    "requires": "./requires.js",
+    "patterns": [ "**/*.md" ],
+    "options": {
+      "useGitIgnore": true,
+      "ignore": [ "node_modules/**/*" ]
+    }
+  }
+}
+```
+
+User can place the configuration anywhere and specify the file location by setting the `MD_LINT_CONFIG_PATH` environment variable. E.g
+
+```bash
+# export MD_LINT_CONFIG_PATH=./open-md-lint.json
+# npm run md-lint
+```
+
+#### Customer md-lint rules
+
+User can specify which checks are enabled. This option requires a js file, for example:
+
+```javascript
+module.exports = [
+  require('remark-lint-final-newline'),
+  require('remark-lint-list-item-bullet-indent')
+]
+```
+
+The js file can be placed in any location and specified by the `requires` field in the configuration. If it is a relative path, the current path is automatically added to form an absolute path.
---
+++ src/index.ts
@@ -0,0 +1,99 @@
+#!/usr/bin/env node
+
+import { readFileSync, existsSync } from 'fs';
+import { join } from 'path';
+import getMarkdownLint from './default_requires';
+// tslint:disable-next-line: no-var-requires
+const deglob = require('deglob');
+// tslint:disable-next-line: no-var-requires
+const gutil = require('gulp-util');
+
+const moduleName = 'open-md-lint';
+
+interface Config {
+  requires?: string;
+  patterns: string[];
+  options: {
+    useGitIgnore?: boolean;
+    usePackageJson?: boolean;
+    configKey?: string;
+    gitIgnoreFile?: string;
+    ignore?: string[];
+    cwd?: string;
+  };
+}
+
+function loadConfig(): Config {
+  const defaultConfig: Config = {
+    patterns: [ '**/*.md' ],
+    options: {
+      useGitIgnore: true,
+      ignore: [ 'node_modules/**/*' ],
+    },
+  };
+
+  const configFile = process.env.MD_LINT_CONFIG_PATH;
+  if (!configFile) {
+    gutil.log(moduleName, gutil.colors.yellow('Not set env MD_LINT_CONFIG_PATH, use default config'));
+    return defaultConfig;
+  }
+  if (!existsSync(configFile)) {
+    gutil.log(moduleName, gutil.colors.yellow(`Can not found config file ${configFile}, use default config`));
+    return defaultConfig;
+  }
+
+  const configs = readFileSync(configFile).toString();
+  if (configs) {
+    const json = JSON.parse(configs);
+    if (json[moduleName]) return json[moduleName];
+  }
+
+  gutil.log(moduleName, gutil.colors.yellow(`Load config from ${configFile} error, use default config`));
+  return defaultConfig;
+}
+
+function getRemarkInstance() {
+  const remark = require('remark');
+  let md_lint_requires = getMarkdownLint();
+
+  // load MarkdownLint
+  if (config.requires) {
+    if (!config.requires.startsWith('/')) {
+      config.requires = join(process.cwd(), config.requires);
+    }
+    if (!existsSync(config.requires)) {
+      gutil.log(moduleName, gutil.colors.yellow(`Can not found md-lint-requires file ${config.requires}, use default config`));
+    } else {
+      gutil.log(moduleName, gutil.colors.yellow(`Load md-lint-requires from ${config.requires}`));
+      md_lint_requires = require(config.requires);
+    }
+  }
+  return remark().use(md_lint_requires);
+}
+
+function doCheck(files: string[]) {
+  const report = require('vfile-reporter');
+  const remarkInstance = getRemarkInstance();
+
+  files.forEach(filePath => {
+    const mdContent = readFileSync(filePath, 'utf8');
+    if (mdContent) {
+      remarkInstance.process(mdContent, (err: any, file: any) => {
+        const res = report(err || file);
+        console.log(filePath);
+        console.log(res);
+        console.log();
+        if (res !== 'no issues found') process.exitCode = -1;
+      });
+    }
+  });
+}
+
+const config = loadConfig();
+deglob(config.patterns, config.options, (err: any, files: string[]) => {
+  if (err) {
+    gutil.log(moduleName, gutil.colors.red(err));
+    process.exit(-1);
+  }
+  doCheck(files);
+});
